5eaddf7dc65a29297fbc45c9ce281c69f141766d
sahil9123 <sahilbhat708@gmail.com>
2024-11-25 23:29:51 +0530

    refactor and on enter

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 "use strict";
-// document.querySelector(".abs_range").textContent = "between";
-// document.querySelector(".right_score").textContent =
-// document.querySelector(".input_box").value = 32;
-// console.log(document.querySelector(".input_box").value);
-
 const buttonListner = document.querySelector(".guess_btn");
-let secretNum = Math.ceil(Math.random() * 20) + 1;
+let getRandomNum = Math.ceil(Math.random() * 20) + 1;
+console.log(getRandomNum);
+let secretNum = getRandomNum;
 let rightScore = 20;
+
+let guess_remark_message = function (message) {
+  document.querySelector(".guess_remark").textContent = message;
+};
+buttonListner.addEventListener("click", handleEvent);
+document.querySelector(".input_box").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    handleEvent();
+  }
+});
 
 document.querySelector(".abs_playgain").addEventListener("click", () => {
   document.querySelector(".main_component").style.backgroundColor = "cyan";
-  document.querySelector(".guess_remark").textContent = "start guessing";
+  guess_remark_message("start guessing");
   document.querySelector(".right_score_num").textContent = 20;
   document.querySelector(".input_box").value = "";
   document.querySelector(".guessed_num").textContent = "?";
@@ -18,58 +25,46 @@ document.querySelector(".abs_playgain").addEventListener("click", () => {
   secretNum = Math.ceil(Math.random() * 20) + 1;
 });
 
-buttonListner.addEventListener("click", () => {
-  console.log(typeof rightScore);
-
+function handleEvent() {
   const guessedNum = Number(document.querySelector(".input_box").value);
 
   if (!guessedNum) {
-    document.querySelector(".guess_remark").textContent = "no number! ";
+    guess_remark_message("no number! ");
   } else if (guessedNum < 1 || guessedNum > 20) {
-    document.querySelector(".guess_remark").textContent = "number out of bound";
+    guess_remark_message("number out of bound");
   } else if (guessedNum > secretNum && guessedNum - secretNum > 3) {
-    document.querySelector(".guess_remark").textContent = "very high";
-    rightScore--;
-    document.querySelector(".right_score_num").textContent = rightScore;
-    if (rightScore < 1) {
-      document.querySelector(".right_score_num").textContent = 0;
-      document.querySelector(".guess_remark").textContent = "u lost the game";
-    }
+    guess_remark_message("very high");
+    rightScoreDecrement();
   } else if (secretNum > guessedNum && secretNum - guessedNum > 3) {
-    document.querySelector(".guess_remark").textContent = "very low";
-    rightScore--;
-    document.querySelector(".right_score_num").textContent = rightScore;
-    if (rightScore < 1) {
-      document.querySelector(".right_score_num").textContent = 0;
-      document.querySelector(".guess_remark").textContent = "u lost the game";
-    }
+    guess_remark_message("very low");
+    rightScoreDecrement();
+    checkGameOver();
   } else if (guessedNum > secretNum) {
-    document.querySelector(".guess_remark").textContent = "bit high";
-    rightScore--;
-    document.querySelector(".right_score_num").textContent = rightScore;
-    if (rightScore < 1) {
-      document.querySelector(".right_score_num").textContent = 0;
-      document.querySelector(".guess_remark").textContent = "u lost the game";
-    }
+    guess_remark_message("bit high");
+    rightScoreDecrement();
+    checkGameOver();
   } else if (guessedNum < secretNum) {
-    document.querySelector(".guess_remark").textContent = "bit low";
-    rightScore--;
-    document.querySelector(".right_score_num").textContent = rightScore;
-    if (rightScore < 1) {
-      document.querySelector(".right_score_num").textContent = 0;
-
-      document.querySelector(".guess_remark").textContent = "u lost the game";
-    }
+    guess_remark_message("bit low");
+    rightScoreDecrement();
+    checkGameOver();
   } else if (guessedNum === secretNum) {
     document.querySelector(".high_score_class").textContent = rightScore;
-    if (rightScore < 1) {
-      document.querySelector(".right_score_num").textContent = 0;
-
-      document.querySelector(".guess_remark").textContent = "u lost the game";
-    }
+    checkGameOver();
     document.querySelector(".right_score_num").textContent = rightScore;
     document.querySelector(".main_component").style.backgroundColor = "#60b347";
     document.querySelector(".guessed_num").textContent = guessedNum;
-    document.querySelector(".guess_remark").textContent = "hurray u win";
+    guess_remark_message("hurray u win");
   }
-});
+}
+
+function checkGameOver() {
+  if (rightScore < 1) {
+    document.querySelector(".right_score_num").textContent = 0;
+    guess_remark_message("u lost the game");
+  }
+}
+
+function rightScoreDecrement() {
+  rightScore--;
+  document.querySelector(".right_score_num").textContent = rightScore;
+}
